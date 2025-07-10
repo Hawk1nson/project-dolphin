c34bc62b2a13e849cc0afae33fd03d645a0770a4
Hawk1nson <andrew.hawkinson@my.metrostate.edu>
2025-07-10 15:05:26 -0500

    Completed FP3 through goal 4

--- celebrations.php
+++ celebrations.php
@@ -1,80 +1,78 @@
-<?php
-if(!isset($_SESSION)) {
-    session_start();
-}
+<!-- 
+    ICS 325 (summer 2025)
+    Final Project
+    Team DOLPHIN  🐬
+-->
 
-require 'db_configuration.php';
-$page_title = 'Project ABCD > Celebrations';
+<?php
 
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-$query = "SELECT * FROM celebrations_tbl";
-$GLOBALS['data'] = mysqli_query($db, $query);
+$page_title = 'Project ABCD > Celebrations';
+
+// Database connection info
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "abcd_db";
+
+// Create connection
+$conn = new mysqli($host, $user, $pass, $db);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Fetch celebrations data
+$sql = "SELECT * FROM celebrations_tbl ORDER BY celebration_date";
+$result = $conn->query($sql);
 ?>
 
 <?php include('header.php'); ?>
 
 <head>
     <link rel="stylesheet" type="text/css" href="css/list_dresses.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@300&display=swap" rel="stylesheet">
 </head>
 
-<br><br>
-<div class="container-fluid">
-    <h2 id="title">Today's Celebrations</h2><br>
-
+<div class="container text-center mt-5">
+    <h2 id="title">Celebrations</h2><br>
+    
     <div id="buttonContainer" style="margin-bottom: 20px;">
         <button><a class="btn btn-sm" href="create_celebration.php">Add a Celebration</a></button>
     </div>
 
-    <div id="customerTableView">
-        <table class="display" id="celebrationsTable" style="width:100%">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Title</th>
-                    <th>Description</th>
-                    <th>Resource Type</th>
-                    <th>Celebration Type</th>
-                    <th>Date</th>
-                    <th>Tags</th>
-                    <th>Image</th>
-                    <th>Resource Link</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php while($row = mysqli_fetch_assoc($GLOBALS['data'])) { ?>
-                    <tr>
-                        <td><?php echo htmlspecialchars($row['id']); ?></td>
-                        <td><?php echo htmlspecialchars($row['title']); ?></td>
-                        <td><?php echo htmlspecialchars($row['description']); ?></td>
-                        <td><?php echo htmlspecialchars($row['resource_type']); ?></td>
-                        <td><?php echo htmlspecialchars($row['celebration_type']); ?></td>
-                        <td><?php echo htmlspecialchars($row['celebration_date']); ?></td>
-                        <td><?php echo htmlspecialchars($row['tags']); ?></td>
-                        <td><img src="images/celebrations/<?php echo htmlspecialchars($row['image_name'] ?: 'celebrations_default.png'); ?>" width="60px"></td>
-                        <td>
-                            <?php if (!empty($row['resource_url'])): ?>
-                                <a href="<?php echo htmlspecialchars($row['resource_url']); ?>" target="_blank">View</a>
-                            <?php else: ?>
-                                N/A
-                            <?php endif; ?>
-                        </td>
-                    </tr>
-                <?php } ?>
-            </tbody>
-        </table>
+    <div class="row justify-content-center">
+        <?php
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                /* pulls image name from DB
+                    make sure to have correct directory structure!
+                    'celebrations_default.png' is whatever the default image you want to use
+                */
+                $imagePath = "images/celebrations/" . ($row['image_name'] ?? 'celebrations_default.png'); // in DB, if 'image_name' is NULL, then will use 'celebrations_default.png' as default image
+                $title = htmlspecialchars($row['title']);
+                $description = htmlspecialchars($row['description']);
+                $id = $row['id'];
+                echo <<<HTML
+                <div class="col-md-3 col-sm-6 mb-4">
+                    <div class="card h-100 shadow">
+                        <img src="$imagePath" class="card-img-top img-fluid" alt="$title">
+                        <div class="card-body">
+                            <h5 class="card-title">$title</h5>
+                            <p class="card-text">$description</p>
+                            <a href="display_celebration.php?id=$id" class="btn btn-primary">View</a>
+                        </div>
+                    </div>
+                </div>
+                HTML;
+            }
+        } else {
+            echo "<p>No celebrations found.</p>";
+        }
+        ?>
     </div>
 </div>
 
-<!-- DataTables and jQuery scripts -->
-<script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
-<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
-<script>
-    $(document).ready(function () {
-        $('#celebrationsTable').DataTable();
-    });
-</script>
+<?php include('footer.php'); ?>
